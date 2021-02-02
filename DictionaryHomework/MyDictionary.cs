21f711a8747e4b22fcf611a6f691d295086b570f
Fatma Ikış <fatmaikis1999@gmail.com>
2021-02-02 17:15:32 +0300

    Add project files.

---
+++ DictionaryHomework/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryHomework
+{
+    class MyDictionary<K,V>
+    {
+        K[] array1;
+        K[] tempArray1;
+        V[] array2;
+        V[] tempArray2;
+        
+        public MyDictionary()
+        {
+
+            array1 = new K[0];
+            array2 = new V[0];
+   
+        }
+        public void Add(K item1, V item2 )
+        {
+            tempArray1 = array1;
+            
+
+            array1 = new K[array1.Length + 1];
+            
+
+            for (int  i= 0; i < tempArray1.Length; i++)
+            {
+                array1[i] = tempArray1[i];
+               
+            }
+
+            array1[array1.Length - 1] = item1;
+
+
+            tempArray2 = array2;
+            array2 = new V[array2.Length + 1];
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                array2[i] = tempArray2[i];
+
+            }
+
+            array2[array2.Length - 1] = item2;
+            Console.WriteLine(item1 + " " + item2);
+
+        }
+
+        
+
+    }
+}
---
+++ DictionaryHomework/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace DictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+
+            Ogrenci.Add(135, "Fatma Ikış ");
+            Ogrenci.Add(230, "Esra Yılmaz");
+            Ogrenci.Add(520, "Elif Nur Aktaş");
+            Ogrenci.Add(780, "Can Tilki");
+            Ogrenci.Add(1002, "Asaf Ağdepe");
+            Ogrenci.Add(1256, "Dilay Çevik");
+            Ogrenci.Add(1445, "Selim Çelikkıran");
+        }
+    }
+}
